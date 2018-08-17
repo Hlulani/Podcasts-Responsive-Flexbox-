5f1875a7625f0e8d8e552c12509cb72d0a9f38d4
Hlulani Baloyi <hlulani@generation.org>
2018-08-17 01:56:37 -0500

    Added s hamburger menu using flex

--- src/MainLayout/MainLayout.js
+++ src/MainLayout/MainLayout.js
@@ -1,18 +1,37 @@
 import React from "react";
 import "./MainLayout.css";
+import "font-awesome/css/font-awesome.min.css";
+import VerticalMenu from "./VerticalMenu";
 
 class MainLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shown: true
+    };
+  }
+
+  showAndHideMenu = () => {
+    this.setState({
+      shown: !this.state.shown
+    });
+  };
+
   render() {
     return (
-      <div className="wrapper">
-        <nav className="nav">
-          <ul>
+      <div className="wrapper flexColumn flexHeight whiteBackground">
+        <nav className="wrapper">
+          <ul className="nav wrapper flexEnd">
+            <li className="active">Home</li>
             <li>About</li>
             <li>Podcasts</li>
             <li>Contact</li>
+            <li className="icon" onClick={this.showAndHideMenu}>
+              <i className="fa fa-bars" />
+            </li>
           </ul>
         </nav>
-
+        <VerticalMenu shown={!this.state.shown} />
         <div id="main">
           <h1> 1. Podcasts 101!</h1>
           <p>
